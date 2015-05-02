c35cb48402c67d2d5e6f8cf1cbd898a5b7239455
Mehta, Dhrumil <dhrumil.mehta@gmail.com>
2015-05-02 10:23:36 -0400

    only use 2013_2014 data

--- download.sh
+++ download.sh
@@ -5,19 +5,19 @@ mkdir -p $FILEPATH
 
 cd $FILEPATH
 
-wget 'ftp://ftp.fec.gov/FEC/2016/indiv16.zip'
-wget 'ftp://ftp.fec.gov/FEC/2016/cn16.zip'
-wget 'ftp://ftp.fec.gov/FEC/2016/ccl16.zip'
-wget 'ftp://ftp.fec.gov/FEC/2016/oth16.zip'
-wget 'ftp://ftp.fec.gov/FEC/2016/pas216.zip'
-wget 'ftp://ftp.fec.gov/FEC/2016/opexp16.zip'
+wget 'ftp://ftp.fec.gov/FEC/2014/indiv14.zip'
+wget 'ftp://ftp.fec.gov/FEC/2014/cn14.zip'
+wget 'ftp://ftp.fec.gov/FEC/2014/ccl14.zip'
+wget 'ftp://ftp.fec.gov/FEC/2014/oth14.zip'
+wget 'ftp://ftp.fec.gov/FEC/2014/pas214.zip'
+wget 'ftp://ftp.fec.gov/FEC/2014/opexp14.zip'
 
 wget -O 'CampaignAndCommitteeSummary.csv' 'http://www.fec.gov/data/CampaignAndCommitteeSummary.do?format=csv'
 
 unzip '*.zip'
 rm *.zip
 
-YEARS="_2015_2016"
+YEARS="_2013_2014"
 mv 'itcont.txt' 'itcont.txt'$YEARS'.txt'
 mv 'itoth.txt' 'itoth'$YEARS'.txt'
 mv 'cm.txt' 'cm'$YEARS'.txt'
