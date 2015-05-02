3c721b70bf82d8436fc01f313d1add9563d32679
Mehta, Dhrumil <dhrumil.mehta@gmail.com>
2015-05-02 01:34:49 -0400

    fix year on downloader of file

--- downloader.py
+++ downloader.py
@@ -3,9 +3,6 @@ Check the FEC website to find if a new filing has been posted.
 If there is a new filing store it in /data folder with new date.
 
 http://www.fec.gov/finance/disclosure/ftpdet.shtml
-
-indiv16.zip 
-oth16.zip
 """
 
 from app import root
@@ -26,7 +23,7 @@ def download_files(new_folder_path):
     '''
     subprocess.call(['sh', root+'/download.sh', new_folder_path])
     download_files
-    
+
 def already_downloaded(filepath):
     '''
     Return true if we already have this version of the file
@@ -46,14 +43,22 @@ def download():
     new_folder_path = "data/downloaded_%s" % date_str
     download_files(new_folder_path)
 
+
     # Delete if already exist in database
     for path, subdirs, files in os.walk(new_folder_path):
-        for f in files:   
+        for f in files:
             if already_downloaded(path + '/' + f):
                 print "Didn't save '%s' because it was already in the database." % f
                 os.remove(path + '/' + f)
             else:
                 print "Saved new file '%s/%s'" % (path, f)
 
+                File.create(
+                    name = f,
+                    years=next(re.finditer(r'\d{4}_\d{4}', f)),
+                    sha1 = sha1OfFile(path + '/' + f),
+                    updated = date.today(),
+                    ingested = False
+                )
 
 download()
